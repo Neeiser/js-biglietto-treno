2a4726412fd988fd98c534806ed74b0f8b04328b
Neeiser <stortini.study@gmail.com>
2022-02-21 16:06:59 +0100

    Finished

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 /* Variable Declaration: Constant */
-const oldUser = 65;
-const youngUser = 18;
-const ticketPrice = 0.21;
+let oldUser = 65;
+let youngUser = 18;
+let ticketPrice = 0.21;
 
 
 /* Variable Declaration: let Prompt */
-let userName= prompt("Inserisci il tuo nome");
+let username= prompt("Inserisci il tuo nome");
 let km= prompt("Inserisci i km");
 let age= prompt("Inserisci la tua età");
 
 /* Variable Declaration: let Operational Result */
 let ticketTotal= km * ticketPrice;
+let ticketOldDiscount= Math.round(ticketTotal*40 / 100);
+let ticketYoungDiscount= Math.round(ticketTotal*20 / 100);
 
 /* Variable HTML print */
 let userOutput = document.getElementById("user-result");
@@ -19,16 +21,16 @@ let userOutput = document.getElementById("user-result");
 /* Function */
 if (age >= oldUser) {
     userOutput.innerHTML =
-    `Ciao ${userName}. Il costo totale del tuo biglietto è di: 
-    ${ticketTotal / 0.4}€`;
+    `Ciao ${username}. Il costo totale del tuo biglietto è di: 
+    ${ticketTotal - ticketOldDiscount}€`;
 
 } else if(age <= youngUser) {
     userOutput.innerHTML =
-    `Ciao ${userName}. Il costo totale del tuo biglietto è di: 
-    ${ticketTotal / 0.2}€`;
+    `Ciao ${username}. Il costo totale del tuo biglietto è di: 
+    ${ticketTotal - ticketYoungDiscount}€`;
 
 } else {
     userOutput.innerHTML =
-    `Ciao ${userName}. Il costo totale del tuo biglietto è di: 
-    ${km * ticketPrice}€`;
+    `Ciao ${username}. Il costo totale del tuo biglietto è di: 
+    ${ticketTotal}€`;
 }
