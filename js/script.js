b041726dfdd3c87e9c8ae22ede44ef09f0b5a8b3
Neeiser <stortini.study@gmail.com>
2022-02-21 15:13:40 +0100

    let fix

--- js/script.js
+++ js/script.js
@@ -9,6 +9,8 @@ let userName= prompt("Inserisci il tuo nome");
 let km= prompt("Inserisci i km");
 let age= prompt("Inserisci la tua età");
 
+/* Variable Declaration: let Operational Result */
+let ticketTotal= km * ticketPrice;
 
 /* Variable HTML print */
 let userOutput = document.getElementById("user-result");
@@ -18,12 +20,12 @@ let userOutput = document.getElementById("user-result");
 if (age >= oldUser) {
     userOutput.innerHTML =
     `Ciao ${userName}. Il costo totale del tuo biglietto è di: 
-    ${km * ticketPrice}€`;
+    ${ticketTotal / 0.4}€`;
 
 } else if(age <= youngUser) {
     userOutput.innerHTML =
     `Ciao ${userName}. Il costo totale del tuo biglietto è di: 
-    ${km * ticketPrice}€`;
+    ${ticketTotal / 0.2}€`;
 
 } else {
     userOutput.innerHTML =
