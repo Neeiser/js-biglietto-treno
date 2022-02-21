e0e84a58c226c06015b9fd6934534fc18edefbec
Neeiser <stortini.study@gmail.com>
2022-02-21 14:59:11 +0100

    Base Js Function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
-/* Variable Declaration */
+/* Variable Declaration: Constant */
+const oldUser = 65;
+const youngUser = 18;
+const ticketPrice = 0.21;
 
-let ticketPrice = 0.21;
-let oldDiscout = 40;
-let youngDiscout = 20;
 
+/* Variable Declaration: let Prompt */
+let userName= prompt("Inserisci il tuo nome");
 let km= prompt("Inserisci i km");
 let age= prompt("Inserisci la tua età");
+
+
+/* Variable HTML print */
+let userOutput = document.getElementById("user-result");
+
+
+/* Function */
+if (age >= oldUser) {
+    userOutput.innerHTML =
+    `Ciao ${userName}. Il costo totale del tuo biglietto è di: 
+    ${(km * ticketPrice)}€`;
+
+} else if(age <= youngUser) {
+    userOutput.innerHTML =
+    `Ciao ${userName}. Il costo totale del tuo biglietto è di: 
+    ${km * ticketPrice}€`;
+    
+} else {
+    userOutput.innerHTML =
+    `Ciao ${userName}. Il costo totale del tuo biglietto è di: 
+    ${km * ticketPrice}€`;
+}
